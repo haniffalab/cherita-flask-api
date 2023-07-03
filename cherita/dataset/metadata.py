187d59898d753d14aab0b345a18f99d82aa15e23
dannda <dann.3004@gmail.com>
2023-07-03 16:34:36 +0100

    update VarNames endpoint to return list of dicts with name and matrix index

--- cherita/dataset/metadata.py
+++ cherita/dataset/metadata.py
@@ -1,5 +1,5 @@
 import zarr
-import anndata as ad
+import pandas as pd
 from cherita.utils.adata_utils import get_group_index_name, parse_data
 
 
@@ -14,7 +14,10 @@ def get_var_col_names(adata_group: zarr.Group):
 
 
 def get_var_names(adata_group: zarr.Group, col: str = None):
+    COL_NAME = "name"
+    INDEX_NAME = "matrix_index"
     col = col or get_group_index_name(adata_group)
-    var_names = list(parse_data(adata_group.var[col]))
-    var_names.sort()
-    return var_names
+    var_df = pd.DataFrame(parse_data(adata_group.var[col]), columns=[COL_NAME])
+    var_df.reset_index(names=[INDEX_NAME], inplace=True)
+    var_df.sort_values(by=[COL_NAME], inplace=True)
+    return var_df.to_dict("records", index=True)
