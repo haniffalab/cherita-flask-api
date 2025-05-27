1862f740925f2ec5f79844e598a7b59c30a910ac
dannda <dann.3004@gmail.com>
2025-05-27 16:12:22 +0100

    validate obs colors in uns are hex triplet strings

--- cherita/dataset/metadata.py
+++ cherita/dataset/metadata.py
@@ -71,12 +71,22 @@ def get_obs_bin_data(
     return {**type_category(pd.Series(cat)), "type": "continuous"}
 
 
-def get_obs_colors(adata_group: zarr.Group, obs_col: str):
+def is_hex_triplet(color: str) -> bool:
+    if not isinstance(color, str):
+        return False
+    return bool(re.fullmatch(r"^#[0-9a-fA-F]{6}$", color))
+
+
+def get_obs_colors(adata_group: zarr.Group, obs_col: str) -> list[str] | None:
     # Following cxg schema https://github.com/chanzuckerberg/single-cell-curation/blob/main/schema/6.0.0/schema.md#column_colors
     obs_colors = f"{obs_col}_colors"
     if obs_colors in adata_group.uns.array_keys():
         try:
-            return parse_data(adata_group.uns[obs_colors]).tolist()
+            colors = parse_data(adata_group.uns[obs_colors]).tolist()
+            if not all(is_hex_triplet(c) for c in colors):
+                logging.warn(f"Colors in {obs_colors} are not hex strings")
+                return None
+            return colors
 
         except Exception as e:
             logging.error(f"Failed to parse colors for {obs_col}: {e}")
